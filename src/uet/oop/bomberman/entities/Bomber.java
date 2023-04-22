8b6b2cf0aefb2a9dc9538736014e9e5fafe7543d
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-22 17:44:59 +0700

    first

---
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -0,0 +1,24 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Bomber extends Entity {
+
+    public Bomber(int x, int y, Image img) {
+        super( x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
