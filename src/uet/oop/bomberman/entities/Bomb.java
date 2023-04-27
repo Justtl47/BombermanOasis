777a1a09c4f7e3175febffa31fdde4d33173e88c
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:49:26 +0700

    add bomb

---
+++ src/uet/oop/bomberman/entities/Bomb.java
@@ -0,0 +1,36 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class Bomb extends Entity {
+    private int timeCount = 0;
+
+    private int radius;
+
+    public Bomb(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+        setLayer(2);
+        radius = 1;
+    }
+
+    public Bomb(int xUnit, int yUnit, Image img, int radius) {
+        super(xUnit, yUnit, img);
+        this.radius = radius;
+        setLayer(2);
+    }
+
+    @Override
+    public void update() {
+        BombermanGame.bomberman.bombable = false;
+        if (timeCount++ == 100) {
+
+        }
+        img = Sprite.movingSprite(Sprite.bomb, Sprite.bomb_1, Sprite.bomb_2, timeCount, 40).getFxImage();
+    }
+
+    public void explodeUpgrade() {
+        Flame e = new Flame(x, y);
+    }
+}
