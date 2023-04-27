aab42b43218a5ee2e363d1b675dd9a56010420e3
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:55:27 +0700

    update Item

--- src/uet/oop/bomberman/entities/Item.java
+++ src/uet/oop/bomberman/entities/Item.java
@@ -1,2 +1,12 @@
-package uet.oop.bomberman.entities;public class Item {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public abstract class Item extends StillEntity {
+    public Item(int x, int y, Image img) {
+        super(x, y, img);
+        setLayer(1);
+    }
+
+    public abstract void change();
 }
