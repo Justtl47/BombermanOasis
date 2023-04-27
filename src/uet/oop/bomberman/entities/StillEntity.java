f93e909f01cec7cf568c38bc306720b08caf623f
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:56:28 +0700

    update StillEntity

--- src/uet/oop/bomberman/entities/StillEntity.java
+++ src/uet/oop/bomberman/entities/StillEntity.java
@@ -1,2 +1,10 @@
-package uet.oop.bomberman.entities;public class StillEntity {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public abstract class StillEntity extends Entity {
+
+    public StillEntity(int x, int y, Image img) {
+        super(x, y, img);
+    }
 }
