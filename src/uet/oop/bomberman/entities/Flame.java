c58eea34ed1412b771611d54e9a23497bce0b949
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:54:32 +0700

    update Flame

--- src/uet/oop/bomberman/entities/Flame.java
+++ src/uet/oop/bomberman/entities/Flame.java
@@ -1,2 +1,20 @@
-package uet.oop.bomberman.entities;public class Flame {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+
+public class Flame extends Entity {
+
+    public Flame(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    public Flame(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
