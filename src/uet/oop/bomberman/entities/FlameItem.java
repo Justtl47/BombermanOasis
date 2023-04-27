d67b33ecb292c58729d3f87daf71d2981a33c78d
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:55:00 +0700

    update FlameItem

--- src/uet/oop/bomberman/entities/FlameItem.java
+++ src/uet/oop/bomberman/entities/FlameItem.java
@@ -1,2 +1,21 @@
-package uet.oop.bomberman.entities;public class FlameItem {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+
+public class FlameItem extends Item {
+
+    public FlameItem(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void change() {
+        BombermanGame.bomberman.setRadius(2);
+    }
 }
