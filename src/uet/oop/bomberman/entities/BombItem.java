d34f4b4777645e79bb96331eec431aac35eb4167
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:53:06 +0700

    add BombItem

--- src/uet/oop/bomberman/entities/BombItem.java
+++ src/uet/oop/bomberman/entities/BombItem.java
@@ -1,2 +1,21 @@
-package uet.oop.bomberman.entities;public class BombItem {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+
+public class BombItem extends Item {
+    public BombItem(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+        setLayer(1);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void change() {
+        BombermanGame.bomberman.infibomb = true;
+    }
 }
