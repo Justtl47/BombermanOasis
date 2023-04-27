bae955b33ce63a1add3d94b53372268ddf1bd59a
SmurfHandle <phamvantruong2442004@gmail.com>
2023-04-27 16:56:00 +0700

    update SpeedItem

--- src/uet/oop/bomberman/entities/SpeedItem.java
+++ src/uet/oop/bomberman/entities/SpeedItem.java
@@ -1,2 +1,22 @@
-package uet.oop.bomberman.entities;public class SpeedItem {
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+
+public class SpeedItem extends Item {
+
+    public SpeedItem(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void change() {
+        int i = BombermanGame.bomberman.getSpeed();
+        BombermanGame.bomberman.setSpeed(i + 1);
+    }
 }
